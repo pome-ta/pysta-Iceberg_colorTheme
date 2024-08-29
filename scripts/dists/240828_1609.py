b99e24269d082d4ff98a9ef37a80a8da96fc9532
pome-ta <53405097+pome-ta@users.noreply.github.com>
2024-08-29 13:12:28 +0900

    up

--- scripts/dists/240828_1609.py
+++ scripts/dists/240828_1609.py
@@ -4,9 +4,9 @@ from objc_util import ObjCClass
 
 themes_path = Path(str(ObjCClass('PA2UITheme').sharedTheme().userThemesPath()))
 
-target_path = Path('./dumps/tmpMergeDumps.json')
+target_path = Path('./dumps/myOceanic.json')
 
 move_path = Path(themes_path, 'hoge.json')
 
-#json_str = target_path.read_text()
-move_path.write_text('{"name": "hoge"}', encoding='utf-8')
+json_str = target_path.read_text()
+move_path.write_text(json_str, encoding='utf-8')
