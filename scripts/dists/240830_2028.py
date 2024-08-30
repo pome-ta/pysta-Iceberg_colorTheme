88c305e9212a7ddbfdfd5234734cca09f7e1d1ca
pome-ta <53405097+pome-ta@users.noreply.github.com>
2024-08-30 21:24:59 +0900

    up

--- scripts/dists/240830_2028.py
+++ scripts/dists/240830_2028.py
@@ -8,7 +8,7 @@ from pprint import pprint
 
 
 def get_json2dict(path:Path)->dict:
-    return json.loads(path.read_text())
+  return json.loads(path.read_text())
 
 _main_keys = []
 _scopes_keys = []
@@ -23,10 +23,10 @@ dumps_path = Path(root_path.parent, 'dumps')
 
 
 for p in dumps_path.iterdir():
-    # print(p.name)
-    json_dict = get_json2dict(p)
-    _main_keys.extend(list(json_dict.keys()))
-    _scopes_keys.extend(list(json_dict['scopes'].keys()))
+  print(p.name)
+  json_dict = get_json2dict(p)
+  _main_keys.extend(list(json_dict.keys()))
+  _scopes_keys.extend(list(json_dict['scopes'].keys()))
 
 
 
@@ -39,10 +39,10 @@ count_scopes_dict = {k:0 for k in max_scopes_keys}
 
 
 for k in _main_keys:
-    count_main_dict[k] += 1
+  count_main_dict[k] += 1
 
 for k in _scopes_keys:
-    count_scopes_dict[k] += 1
+  count_scopes_dict[k] += 1
 
 min_main_keys = [k for k,v in count_main_dict.items() if v == dumps_len]
 min_scopes_keys = [k for k,v in count_scopes_dict.items() if v == dumps_len]
@@ -51,21 +51,22 @@ min_scopes_keys = [k for k,v in count_scopes_dict.items() if v == dumps_len]
 
 print('- max_main_keys')
 for k in max_main_keys:
-    print(f'  - {k}')
+  print(f'  - {k}')
 
 print('')
 print('- max_scopes_keys')
 for k in max_scopes_keys:
-    print(f'  - {k}')
+  print(f'  - {k}')
 
 print('')
 print('- min_main_keys')
 for k in min_main_keys:
-    print(f'  - {k}')
+  print(f'  - {k}')
 
 print('')
 print('- min_scopes_keys')
 for k in min_scopes_keys:
-    print(f'  - {k}')
+  print(f'  - {k}')
 
 x = 1
+
