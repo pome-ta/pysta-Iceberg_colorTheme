2926ddddd5cf62edcd7fbb61f5e225bd9529c7fa
pome-ta <53405097+pome-ta@users.noreply.github.com>
2024-08-30 23:08:15 +0900

    set up

---
+++ scripts/dists/240830_2124.py
@@ -0,0 +1,71 @@
+from pathlib import Path
+import json
+
+theme_dict = {
+  'font-family': 'Menlo-Regular',
+  'font-size': 10,
+}
+
+
+scopes = {
+  'bold': {
+    'font-style': 'bold',
+  },
+  'bold-italic': {
+    'font-style': 'bold-italic',
+  },
+  'checkbox': {
+    'checkbox': True,
+  },
+  'checkbox-done': {
+    'checkbox': True,
+    'done': True,
+  },
+  'heading-1': {
+    'font-style': 'bold',
+  },
+  'heading-2': {
+    'font-style': 'bold',
+  },
+  'heading-3': {
+    'font-style': 'bold',
+  },
+  'italic': {
+    'font-style': 'italic',
+  },
+  'link': {
+    'text-decoration': 'underline',
+  },
+  'bold-italic': {
+    'font-style': 'bold-italic',
+  },
+  'project': {
+    'font-style': 'bold',
+  },
+  'tag': {
+    'text-decoration': 'none',
+  },
+  'task-done': {
+    'text-decoration': 'strikeout',
+  },
+  'classdef': {
+    'font-style': 'bold',
+  },
+  'code': {
+    'corner-radius': 2,
+  },
+  'comment': {
+    'font-style': 'italic',
+  },
+  'docstring': {
+    'font-style': 'italic',
+  },
+  'functiondef': {
+    'font-style': 'bold',
+  },
+}
+
+theme_dict.update({
+  'scopes': scopes,
+})
+
