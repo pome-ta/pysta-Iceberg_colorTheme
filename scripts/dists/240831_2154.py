29415e147fca7071042dfb964375279b1ec6e277
pome-ta <53405097+pome-ta@users.noreply.github.com>
2024-08-31 23:11:46 +0900

    up

---
+++ scripts/dists/240831_2154.py
@@ -0,0 +1,9 @@
+scopes = {
+  'classdef': {
+    'font-style': 'bold',
+  },
+}
+
+#scopes |= {'classdef': {'color':'#000000'}}
+
+scopes.setdefault('classdef',{'color':'#000000'})
---
+++ scripts/dists/240831_2219.py
@@ -0,0 +1,11 @@
+class Scopes:
+
+  def __init__(self):
+    pass
+
+
+class Theme:
+
+  def __init__(self):
+    pass
+
