7eae362a832bd2273d435533fdb649636e4d3477
pome-ta <53405097+pome-ta@users.noreply.github.com>
2024-08-31 23:49:07 +0900

    up

--- scripts/dists/240831_2219.py
+++ scripts/dists/240831_2219.py
@@ -1,11 +1,29 @@
-class Scopes:
+from pathlib import Path
+import json
+from collections import namedtuple
+from types import SimpleNamespace
 
-  def __init__(self):
-    pass
+class DictDotNotation(dict): 
+  def __init__(self, *args, **kwargs): 
+    super().__init__(*args, **kwargs) 
+    self.__dict__ = self 
 
+# class DictDotNotation(dict):
+#   def __getattr__(self, key):
+#     return self.__getitem__(key)
+  
+#   def __setattr__(self, key, value):
+#     self.__setitem__(key, value)
 
-class Theme:
+#   def __delattr__(self, key):
+#     self.__delitem__(key)
 
-  def __init__(self):
-    pass
+vscode_theme_path = Path(
+  Path(__file__).parent, './vscodeThemes/iceberg.color-theme.json')
+vscode_theme_dict = json.loads(vscode_theme_path.read_text())
 
+theme = DictDotNotation(vscode_theme_dict)
+# print(theme.colors['list.hoverBackground'])
+# ns = SimpleNamespace(**vscode_theme_dict)
+
+x = 1
