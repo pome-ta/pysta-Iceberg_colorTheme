b2d939cdbb5485b86885227aad8e8f80e98ae1a0
pome-ta <53405097+pome-ta@users.noreply.github.com>
2024-09-01 22:26:07 +0900

    up

--- scripts/dists/240901_1856.py
+++ scripts/dists/240901_1856.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 import json
 
+from dataclasses import dataclass
+
 vscode_theme_path = Path(
   Path(__file__).parent, './vscodeThemes/iceberg.color-theme.json')
 vscode_theme_dict = json.loads(vscode_theme_path.read_text())
@@ -41,7 +43,7 @@ def get_vsdict_value(items: list, theme_main: dict) -> str | bool | None:
       return value
 
 
-# todo: 辞書生成もっとシュッとできるといいけど、、、
+# todo: 辞書生成もっとシュッとできるといいけど、、、
 _background = [
   dict(colors='editor.background'),
 ]
@@ -214,5 +216,83 @@ _tint = [
 set_keys = {
   'background': _background,
   'bar_background': _bar_background,
+  'default_text': _default_text,
+  'gutter_background': _gutter_background,
+  'gutter_border': _gutter_border,
+  'library_background': _library_background,
+  'line_number': _line_number,
+  'name': _name,
+  'scopes_builtinfunction_color': _scopes_builtinfunction_color,
+  'scopes_class_color': _scopes_class_color,
+  'scopes_classdef_color': _scopes_classdef_color,
+  'scopes_code_backgroundColor': _scopes_code_backgroundColor,
+  'scopes_codeblockStart_color': _scopes_codeblockStart_color,
+  'scopes_comment_color': _scopes_comment_color,
+  'scopes_default_color': _scopes_default_color,
+  'scopes_docstring_color': _scopes_docstring_color,
+  'scopes_formatting_color': _scopes_formatting_color,
+  'scopes_function_color': _scopes_function_color,
+  'scopes_functiondef_color': _scopes_functiondef_color,
+  'scopes_keyword_color': _scopes_keyword_color,
+  'scopes_module_color': _scopes_module_color,
+  'scopes_number_color': _scopes_number_color,
+  'scopes_string_color': _scopes_string_color,
+  'scopes_taskDone_color': _scopes_taskDone_color,
+  'separator_line': _separator_line,
+  'tab_background': _tab_background,
+  'tab_title': _tab_title,
+  'text_selection_tint': _text_selection_tint,
+  'thumbnail_border': _thumbnail_border,
+  'tint': _tint,
 }
+
+convert_dict = {
+  k: get_vsdict_value(v, vscode_theme_dict)
+  for k, v in set_keys.items()
+}
+
+
+@dataclass
+class DataPalette:
+  background: str = '#ff0000'
+  bar_background: str = '#ff0000'
+  dark_keyboard: bool = True
+  default_text: str = '#ff0000'
+  gutter_background: str = '#ff0000'
+  gutter_border: str = '#ff0000'
+  library_background: str = '#ff0000'
+  library_tint: str = '#ff0000'
+  line_number: str = '#ff0000'
+  name: str = 'templateDefaultThemeSample'
+
+  scopes_builtinfunction_color: str = '#ff0000'
+  scopes_class_color: str = '#ff0000'
+  scopes_classdef_color: str = '#ff0000'
+  scopes_code_backgroundColor: str = '#ff0000'
+  scopes_codeblockStart_color: str = '#ff0000'
+  scopes_comment_color: str = '#ff0000'
+  scopes_default_color: str = '#ff0000'
+  scopes_docstring_color: str = '#ff0000'
+  scopes_formatting_color: str = '#ff0000'
+  scopes_function_color: str = '#ff0000'
+  scopes_functiondef_color: str = '#ff0000'
+  scopes_keyword_color: str = '#ff0000'
+  scopes_module_color: str = '#ff0000'
+  scopes_number_color: str = '#ff0000'
+  scopes_string_color: str = '#ff0000'
+  scopes_taskDone_color: str = '#ff0000'
+
+  separator_line: str = '#ff0000'
+  tab_background: str = '#ff0000'
+  tab_title: str = '#ff0000'
+  text_selection_tint: str = '#ff0000'
+  thumbnail_border: str = '#ff0000'
+  tint: str = '#ff0000'
+
+
+def create_theme():
+  pass
+
+
+aa = DataPalette(**convert_dict)
 
