8d74b00de98c25f61bff49aae72f207672186a8e
pome-ta <53405097+pome-ta@users.noreply.github.com>
2024-09-03 22:00:59 +0900

    Update 240903_1758.py

--- scripts/dists/240903_1758.py
+++ scripts/dists/240903_1758.py
@@ -1,7 +1,32 @@
 from pathlib import Path
+import json
 
-theme_path = Path(Path(__file__).parent, './vscodeThemes')
+themes_path = Path(Path(__file__).parent, './vscodeThemes')
+
+# for p in theme_path.iterdir():
+#   print(p.name)
+
+theme_path = list(themes_path.iterdir())[0]
+theme_dict = json.loads(theme_path.read_text())
+
+file_name = theme_path.name
 
 
-for p in theme_path.iterdir():
-  print(p.name)
+# todo: `value` を一覧として、初手取り出し
+
+# todo: `key` でのテスト
+# これだと、配列時の処理ができていない
+def allkeys(a, parent=''):
+  for key, value in a.items():
+    yield parent + key
+    if isinstance(value, dict):
+      yield from allkeys(value, parent + key + '.')
+
+
+# print(theme_path.name)
+
+vs = list(allkeys(theme_dict))
+
+
+
+x = 1
