2389a0131fd4863f01f4b636e7d11f0db25bb1d3
pome-ta <53405097+pome-ta@users.noreply.github.com>
2024-09-04 22:18:18 +0900

    up

--- scripts/dists/240904_1618.py
+++ scripts/dists/240904_1618.py
@@ -17,11 +17,11 @@ def get_all_values(vs_theme: dict):
   # color_regex = re.compile(r'^#([\da-fA-F]{6}|[\da-fA-F]{3}||[\da-fA-F]{8})')
   # color_regex = re.compile(r'^#[0-9a-fA-F]{3,6,8}')
   # color_regex = re.compile(r'^#[\da-fA-F]{3,8}')
-  
+
   def _yield_value(value: str | bool | None):
     if isinstance(value, str) and color_regex.match(value):
       yield value.upper()
-  
+
   def _for_type_list(lst: list):
     for v in lst:
       if isinstance(v, dict):
@@ -30,7 +30,7 @@ def get_all_values(vs_theme: dict):
         yield from _for_type_list(v)
       else:
         yield from _yield_value(v)
-  
+
   def _for_type_dict(dct: dict):
     for k, v in dct.items():
       if isinstance(v, dict):
@@ -39,7 +39,7 @@ def get_all_values(vs_theme: dict):
         yield from _for_type_list(v)
       else:
         yield from _yield_value(v)
-  
+
   if isinstance(vs_theme, dict):
     yield from _for_type_dict(vs_theme)
 
@@ -56,23 +56,23 @@ def all_keys(a, parent=''):
 
 
 def get_all_keys(vs_theme: dict):
+
   def _yield_value(value: str | bool | None, parent: str):
     # yield f'{parent + str(value)}'
     # yield parent
     if isinstance(value, str) and color_regex.match(value):
       yield parent
-  
+
   def _for_type_list(lst: list, parent: str):
     for n, v in enumerate(lst):
       if isinstance(v, dict):
         print('in dic')
         # print(v)
         k = v.get('scope')
-        
+
         # yield from _for_type_dict(v, f'{parent}[{k[n] if isinstance(k, list) else k}] | ')
         yield from _for_type_dict(v, f'{parent}[{k}] :: ')
-      
-      
+
       elif isinstance(v, list):
         # xxx: ここのパターン数ない
         print('in list')
@@ -80,7 +80,7 @@ def get_all_keys(vs_theme: dict):
       else:
         # print('in val')
         yield from _yield_value(v, f'{parent}[{v}] :: ')
-  
+
   def _for_type_dict(dct: dict, parent: str):
     for k, v in dct.items():
       if isinstance(v, dict):
@@ -89,7 +89,7 @@ def get_all_keys(vs_theme: dict):
         yield from _for_type_list(v, f'{parent + k} :: ')
       else:
         yield from _yield_value(v, f'{parent + k}')
-  
+
   if isinstance(vs_theme, dict):
     yield from _for_type_dict(vs_theme, '')
 
@@ -101,3 +101,4 @@ all_values = sorted(list(set(get_all_values(theme_dict))))
 all_keys = list(get_all_keys(theme_dict))
 
 x = 1
+
