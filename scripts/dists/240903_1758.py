bb2e6661c4efa6318cba641593d1f7332969ad6b
pome-ta <53405097+pome-ta@users.noreply.github.com>
2024-09-04 14:41:00 +0900

    yield

--- scripts/dists/240903_1758.py
+++ scripts/dists/240903_1758.py
@@ -15,14 +15,45 @@ file_name = theme_path.name
 #color_regex = re.compile(r'^#([\da-fA-F]{6}|[\da-fA-F]{3}||[\da-fA-F]{8})')
 #color_regex = re.compile(r'^#[0-9a-fA-F]{3,6,8}')
 color_regex = re.compile(r'^#[\da-fA-F]{3,8}')
+
+
+def for_type_list(lst: list):
+  for v in lst:
+    if isinstance(v, dict):
+      yield from for_type_dict(v)
+    elif isinstance(v, list):
+      yield from for_type_list(v)
+    else:
+      if isinstance(v, str) and color_regex.match(v):
+        yield v
+
+
+def for_type_dict(dct: dict):
+  for k, v in dct.items():
+    if isinstance(v, dict):
+      yield from for_type_dict(v)
+    elif isinstance(v, list):
+      yield from for_type_list(v)
+    else:
+      if isinstance(v, str) and color_regex.match(v):
+        yield v
+
+
 # todo: `value` を一覧として、初手取り出し
-def get_all_values(d: dict):
-  for k, v in d.items():
-    #print(v)
+def get_all_values(vs_theme: dict):
+  '''
+  for k, v in vs_theme.items():
     if isinstance(v, str) and color_regex.match(v):
-      print(v)
+      yield v
     elif isinstance(v, dict):
-      get_all_values(v)
+      #yield from for_type_dict(v)
+      yield from get_all_values(v)
+      
+    elif isinstance(v, list):
+      yield from for_type_list(v)
+  '''
+  if isinstance(vs_theme, dict):
+    yield from for_type_dict(vs_theme)
 
 
 # todo: `key` でのテスト
@@ -37,6 +68,7 @@ def all_keys(a, parent=''):
 # print(theme_path.name)
 
 #vs = list(all_keys(theme_dict))
-get_all_values(theme_dict)
+a = sorted(list(set(get_all_values(theme_dict))))
 
 x = 1
+
