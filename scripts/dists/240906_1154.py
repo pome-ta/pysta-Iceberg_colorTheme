1b0ae5714ea88bf15f1b412ba2588341bb816ab0
pome-ta <53405097+pome-ta@users.noreply.github.com>
2024-09-06 14:55:54 +0900

    up

--- scripts/dists/240906_1154.py
+++ scripts/dists/240906_1154.py
@@ -11,10 +11,23 @@ def get_target_path(path: Path) -> Path:
   return Path(root_path, path)
 
 
+def get_json_to_dict(json_path: Path) -> dict:
+  text_data = json_path.read_text()
+  try:
+    json_data = json.loads(text_data)
+  except json.decoder.JSONDecodeError as e:
+    regex = re.compile(r'/\*[\s\S]*?\*/|//.*')
+    res_comment_json = regex.sub('', text_data)
+    json_data = json.loads(res_comment_json)
+    print(f'{e}\n\t-> json ファイル内のコメントを除去してデータ作成')
+  return json_data
+  
+
+
 if __name__ == '__main__':
   json_dir = './vscodeThemes'
   vs_theme_jdon_path = get_target_path(json_dir)
-  
+
   for p in vs_theme_jdon_path.iterdir():
-    print(p.name)
+    a = get_json_to_dict(p)
 
