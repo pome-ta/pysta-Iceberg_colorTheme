eadb6decef71ecf0947de755df2ab91258e89f29
pome-ta <53405097+pome-ta@users.noreply.github.com>
2024-09-07 02:59:25 +0900

    up

--- scripts/dists/240906_1154.py
+++ scripts/dists/240906_1154.py
@@ -25,8 +25,27 @@ def get_json_path_to_dict(json_path: Path) -> dict:
 
 
 def get_all_keys(theme_dict: dict):
+
+  def _for_type_list(_list: list, parent: str):
+    for n, v in enumerate(_list):
+      if isinstance(v, dict):
+        yield from _for_type_dict(v, f'{parent}|::')
+      elif isinstance(v, list):
+        yield from _for_type_list(v, f'{parent}|::')
+      else:
+        yield f'{parent}|'
+
+  def _for_type_dict(_dict: dict, parent: str):
+    for k, v in _dict.items():
+      if isinstance(v, dict):
+        yield from _for_type_dict(v, f'{parent + k}::')
+      elif isinstance(v, list):
+        yield from _for_type_list(v, f'{parent + k}::')
+      else:
+        yield f'{parent + k}'
+
   if isinstance(theme_dict, dict):
-    yield theme_dict
+    yield from _for_type_dict(theme_dict, '')
 
 
 def merge_keys_list(theme_path: Path) -> list:
@@ -37,7 +56,8 @@ if __name__ == '__main__':
   json_dir = './vscodeThemes'
   vs_theme_jdon_path = get_target_path(json_dir)
 
-  dmy_theme_path = list(vs_theme_jdon_path.iterdir())[1]
+  dmy_theme_path = list(vs_theme_jdon_path.iterdir())[2]
+  dmy_theme_name = dmy_theme_path.name
   dmy_theme_dict = get_json_path_to_dict(dmy_theme_path)
-  theme_keys_list = list(get_all_keys(dmy_theme_dict))
+  theme_keys_list = sorted(list(set(get_all_keys(dmy_theme_dict))))
 
