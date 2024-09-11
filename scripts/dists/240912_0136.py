13508c9429a34cf95574b8deeb873b205652b960
pome-ta <53405097+pome-ta@users.noreply.github.com>
2024-09-12 01:46:31 +0900

    up

---
+++ scripts/dists/240912_0136.py
@@ -0,0 +1,17 @@
+"""
+note: iceberg 完成を目指す
+pallet で吟味しつつ、設定を固めていく
+"""
+
+from pathlib import Path
+import json
+import re
+
+
+
+if __name__ == '__main__':
+  json_dir = './vscodeThemes'
+  json_name = 'iceberg.color-theme.json'
+
+  
+
