f7bc1cc3d3a3531d4287e78f1201a7fbe816f554
pome-ta <53405097+pome-ta@users.noreply.github.com>
2024-09-13 14:08:57 +0900

    dataclass

--- scripts/dists/240913_0226.py
+++ scripts/dists/240913_0226.py
@@ -6,129 +6,24 @@ note: それぞれの値の適応の仕方を考える
 from pathlib import Path
 import json
 from dataclasses import dataclass, field
-'''
-
-{
- scopes: {
-  bold: {
-   font-style: bold
-  },
-  bold-italic: {
-   font-style: bold-italic
-  },
-  builtinfunction: {
-   color: #ff0000
-  },
-  checkbox: {
-   checkbox: true
-  },
-  checkbox-done: {
-   checkbox: true,
-   done: true
-  },
-  class: {
-   color: #ff0000
-  },
-  classdef: {
-   color: #ff0000,
-   font-style: bold
-  },
-  code: {
-   background-color: #ff0000,
-   corner-radius: 2.0
-  },
-  codeblock-start: {
-   color: #ff0000
-  },
-  comment: {
-   color: #ff0000,
-   font-style: italic
-  },
-  decorator: {
-   color: #ff0000
-  },
-  default: {
-   color: #ff0000
-  },
-  docstring: {
-   color: #ff0000,
-   font-style: italic
-  },
-  escape: {
-   background-color: #ff0000
-  },
-  formatting: {
-   color: #ff0000
-  },
-  function: {
-   color: #ff0000
-  },
-  functiondef: {
-   color: #ff0000,
-   font-style: bold
-  },
-  heading-1: {
-   font-style: bold
-  },
-  heading-2: {
-   font-style: bold
-  },
-  heading-3: {
-   font-style: bold
-  },
-  italic: {
-   font-style: italic
-  },
-  keyword: {
-   color: #ff0000
-  },
-  link: {
-   text-decoration: underline
-  },
-  marker: {
-   box-background-color: #ff0000,
-   box-border-color: #ff0000,
-   box-border-type: 4
-  },
-  module: {
-   color: #ff0000
-  },
-  number: {
-   color: #ff0000
-  },
-  project: {
-   font-style: bold
-  },
-  string: {
-   color: #ff0000
-  },
-  tag: {
-   text-decoration: none
-  },
-  task-done: {
-   color: #ff0000,
-   text-decoration: strikeout
-  }
- },
- 
-}
-
-'''
 
 
-@dataclass
-class BoldTemplate:
-  font_style: str = 'bold'
+def get_target_path(path: Path | str) -> Path:
+  root_path = Path(__file__).parent
+  return Path(root_path, path)
 
 
-@dataclass
-class Bold_ItalicTemplate:
-  font_style: str = 'bold-italic'
-
-
-@dataclass
-class ScopesTemplate:
-  bold: field(default_factory=dict)
+def get_json_path_to_dict(json_path: Path) -> dict:
+  text_data = json_path.read_text()
+  try:
+    json_data = json.loads(text_data)
+  except json.decoder.JSONDecodeError as e:
+    import re
+    regex = re.compile(r'/\*[\s\S]*?\*/|//.*')
+    res_comment_json = regex.sub('', text_data)
+    json_data = json.loads(res_comment_json)
+    print(f'{e}\n\t-> json ファイル内のコメントを除去してデータ作成')
+  return json_data
 
 
 @dataclass
@@ -151,56 +46,52 @@ class ThemeTemplate:
   library_tint: str = '#ff0000'
   line_number: str = '#ff0000'
   name: str = 'tmpFormatDump'
-  
+
   scopes_bold_font_style: str = 'bold'
   scopes_bold_italic_font_style: str = 'bold-italic'
   scopes_builtinfunction_color: str = '#ff0000'
   scopes_checkbox_checkbox: bool = True
   scopes_checkbox_done_checkbox: bool = True
   scopes_checkbox_done_done: bool = True
-  
   scopes_class_color: str = '#ff0000'
   scopes_classdef_color: str = '#ff0000'
-  
   scopes_code_backgroundColor: str = '#ff0000'
   scopes_codeblockStart_color: str = '#ff0000'
+  scopes_decorator_color: str = '#ff0000'
   scopes_comment_color: str = '#ff0000'
   scopes_default_color: str = '#ff0000'
   scopes_docstring_color: str = '#ff0000'
+  scopes_escape_color: str = '#ff0000'
   scopes_formatting_color: str = '#ff0000'
   scopes_function_color: str = '#ff0000'
   scopes_functiondef_color: str = '#ff0000'
+  scopes_heading1_font_style: str = 'bold'
+  scopes_heading2_font_style: str = 'bold'
+  scopes_heading3_font_style: str = 'bold'
+  scopes_italic_font_style: str = 'italic'
   scopes_keyword_color: str = '#ff0000'
+  scopes_link_text_decoration: str = 'underline'
+  scopes_marker_box_background_color: str = '#ff0000'
+  scopes_marker_box_border_color: str = '#ff0000'
+  scopes_marker_box_border: int = 4
   scopes_module_color: str = '#ff0000'
   scopes_number_color: str = '#ff0000'
+  scopes_project_font_style: str = 'bold'
   scopes_string_color: str = '#ff0000'
+  scopes_tag_text_decoration: str = 'none'
   scopes_taskDone_color: str = '#ff0000'
-  
+  scopes_taskDone_text_decoration: str = 'strikeout'
+
   separator_line: str = '#ff0000'
   tab_background: str = '#ff0000'
   tab_title: str = '#ff0000'
   text_selection_tint: str = '#ff0000'
   thumbnail_border: str = '#ff0000'
   tint: str = '#ff0000'
-    
 
 
-def get_target_path(path: Path | str) -> Path:
-  root_path = Path(__file__).parent
-  return Path(root_path, path)
-
-
-def get_json_path_to_dict(json_path: Path) -> dict:
-  text_data = json_path.read_text()
-  try:
-    json_data = json.loads(text_data)
-  except json.decoder.JSONDecodeError as e:
-    import re
-    regex = re.compile(r'/\*[\s\S]*?\*/|//.*')
-    res_comment_json = regex.sub('', text_data)
-    json_data = json.loads(res_comment_json)
-    print(f'{e}\n\t-> json ファイル内のコメントを除去してデータ作成')
-  return json_data
+def create_theme(convert_pallet: dict):
+  p = ThemeTemplate(**convert_pallet)
 
 
 if __name__ == '__main__':
@@ -210,9 +101,8 @@ if __name__ == '__main__':
 
   tmp_dir = './dumps'
   tmp_name = 'tmpFormatDump.json'
-  
-  
-  tsdc = {'background':'#000000'}
+
+  tsdc = {'background': '#000000'}
 
   tt = ThemeTemplate(**tsdc)
 
