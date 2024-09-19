3761aa62e2bece271f263cbec1abdb94296f9371
pome-ta <53405097+pome-ta@users.noreply.github.com>
2024-09-19 22:37:36 +0900

    dump

--- scripts/dists/240919_1529.py
+++ scripts/dists/240919_1529.py
@@ -1,5 +1,6 @@
 """
 note: GitHub 取り込みの処理
+とりま、力技でやってる
   - vs のdump
   - url 経由ではなく、内部dump から
 """
@@ -37,6 +38,7 @@ def get_repository_tokens(github_url: str) -> dict:
   res = requests.get(api_url)
   return res.json()
 
+
 # ref: [Pythonの辞書型(dict)でドットアクセス(dot notation)するメモ](https://zenn.dev/kazuhito/articles/dbe6bbf8ce3ef2)
 class DictDotNotation(dict):
 
@@ -55,16 +57,27 @@ def get_repo_author_license_pushed_at(github_url: str) -> DictDotNotation:
   _pushed_at = tokens.get('pushed_at')
 
   return DictDotNotation({
-    'html_url': _html_url,
-    'author': _author,
-    'license': _license,
-    'pushed_at': _pushed_at,
+    '___html_url': _html_url,
+    '___author': _author,
+    '___license': _license,
+    '___pushed_at': _pushed_at,
   })
 
 
 if __name__ == '__main__':
-  target_url = 'https://github.com/cocopon/vscode-iceberg-theme/blob/main/themes/iceberg.color-theme.json'
+  #target_url = 'https://github.com/cocopon/vscode-iceberg-theme/blob/main/themes/iceberg.color-theme.json'
+  target_url = 'https://github.com/cocopon/vscode-iceberg-theme/blob/main/themes/iceberg-light.color-theme.json'
 
   iceberg_name, iceberg_data = get_vs_theme_data(target_url)
   iceberg_info = get_repo_author_license_pushed_at(target_url)
+
+  iceberg_data.update(iceberg_info)
+  json_str = json.dumps(iceberg_data,
+                        indent=1,
+                        sort_keys=True,
+                        ensure_ascii=False)
+
+  target_path = Path('./vscodeThemes')
+  theme_path = Path(target_path, iceberg_name)
+  theme_path.write_text(json_str, encoding='utf-8')
 
