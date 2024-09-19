f9332d59463f824b27e1a3adadafbc1496d74e26
pome-ta <53405097+pome-ta@users.noreply.github.com>
2024-09-19 22:41:26 +0900

    init

---
+++ scripts/dists/240919_2239.py
@@ -0,0 +1,12 @@
+"""
+note: GitHub 取り込みと、dump 取り込み双方での設計
+"""
+
+from pathlib import Path
+
+import requests
+
+if __name__ == '__main__':
+  #target_url = 'https://github.com/cocopon/vscode-iceberg-theme/blob/main/themes/iceberg.color-theme.json'
+  target_url = 'https://github.com/cocopon/vscode-iceberg-theme/blob/main/themes/iceberg-light.color-theme.json'
+
