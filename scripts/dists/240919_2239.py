862181726be80f5cc7393f763c9df38d776d991d
pome-ta <53405097+pome-ta@users.noreply.github.com>
2024-09-20 14:32:43 +0900

    getter setter

--- scripts/dists/240919_2239.py
+++ scripts/dists/240919_2239.py
@@ -14,38 +14,52 @@ class ThemeInterpretation:
     pass
 
 
-class ThemeData:
+class ThemeObject:
+  """
+  repository の情報をまるっと持つ
+  API 取得と、直のraw 取得の2種類
+  dump してローカルに保存
+  ダンダーでの変数を外側で使うイメージ
+  
+  # 今後
+  API の制限回避としてローカルdump から持ってくるパターンも考えたい
+  """
 
-  def __init__(self, github_url: str, use_local: bool = False):
+  def __init__(self, github_url: str):
+    self._name: str
+    self._data: dict | None
+    self._info: dict | None
 
-    self.use_local = use_local
-    self.__name: str
-    self.__data: dict
-    self.__info: dict
-    self.info_attribute_keys = [
-      '___html_url',
-      '___author',
-      '___license',
-      '___pushed_at',
-    ]
+    self.__setup_attributes(github_url)
 
-    if self.use_local:
-      pass
-    else:
-      tokens = self.__api_tokens(github_url)
+  def __setup_attributes(self, url: str):
+    self.name = url
+    self.data = url
+    self.info = url
 
-      _html_url = tokens.get('html_url')
-      _author = tokens.get('owner').get('login')
-      _license = l.get('name') if (
-        l := tokens.get('license')) is not None else str(l)
-      _pushed_at = tokens.get('pushed_at')
+  @property
+  def name(self) -> str:
+    return self._name
 
-      info = {
-        '___html_url': _html_url,
-        '___author': _author,
-        '___license': _license,
-        '___pushed_at': _pushed_
-      }
+  @name.setter
+  def name(self, url: str):
+    self._name = Path(url).name
+
+  @property
+  def data(self) -> dict | None:
+    return self._data
+
+  @data.setter
+  def data(self, url: str):
+    self._data = self.__get_json_data(url)
+
+  @property
+  def info(self) -> dict | None:
+    return self._info
+
+  @info.setter
+  def info(self, url: str):
+    self._info = self.__get_info_attribute(url)
 
   class DictDotNotation(dict):
 
@@ -53,44 +67,49 @@ class ThemeData:
       super().__init__(*args, **kwargs)
       self.__dict__ = self
 
-  @property
-  def name(self) -> str:
-    return self.__name
+  def __get_json_data(self, url: str) -> dict | None:
+    params = {
+      'raw': 'true',
+    }
+    response = requests.get(url, params)
+    if response.status_code == 200:
+      # xxx: iceberg には、comment ない
+      # wip: comment 削除処理
+      return response.json()
 
-  @name.setter
-  def name(self, name: str):
-    pass
-    
+  def __get_info_attribute(self, url: str) -> DictDotNotation | None:
+    tokens = self.__api_tokens(url)
+    if tokens is None:
+      return
+    _html_url = tokens.get('html_url')
+    _author = tokens.get('owner').get('login')
+    _license = l.get('name') if (l :=
+                                 tokens.get('license')) is not None else str(l)
+    _pushed_at = tokens.get('pushed_at')
 
-  @property
-  def data(self) -> dict:
-    return self.__data
+    # xxx: `_` は3つ
+    info = {
+      '___html_url': _html_url,
+      '___author': _author,
+      '___license': _license,
+      '___pushed_at': _pushed_at,
+    }
 
-  @data.setter
-  def data(self, data: dict):
-    pass
+    return self.DictDotNotation(info)
 
-  @property
-  def info(self) -> dict:
-    return self.__info
-
-  @info.setter
-  def info(self, info: dict):
-    pass
-    
-  def setup_info(self):
-    pass
-
-  def __api_tokens(self, github_url: str) -> dict:
-    _, _, owner_name, repo_name, *_ = Path(github_url).parts
+  def __api_tokens(self, url: str) -> dict | None:
+    _, _, owner_name, repo_name, *_ = Path(url).parts
 
     api_url = f'https://api.github.com/repos/{owner_name}/{repo_name}'
     # wip: 制限かかった時の処理
     response = requests.get(api_url)
-    return response.json()
+    if response.status_code == 200:
+      return response.json()
 
 
 if __name__ == '__main__':
   #target_url = 'https://github.com/cocopon/vscode-iceberg-theme/blob/main/themes/iceberg.color-theme.json'
   target_url = 'https://github.com/cocopon/vscode-iceberg-theme/blob/main/themes/iceberg-light.color-theme.json'
+
+  to = ThemeObject(target_url)
 
