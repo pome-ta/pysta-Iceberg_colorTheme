edd5b64b7568789ab6ec68f85858e27b977f19a8
pome-ta <53405097+pome-ta@users.noreply.github.com>
2024-09-22 00:12:24 +0900

    add comment

--- scripts/dists/240921_2207.py
+++ scripts/dists/240921_2207.py
@@ -12,79 +12,80 @@ import requests
 
 @dataclass
 class ConvertThemeTemplate:
-  url: str = 'github_url'
-  author: str = 'author_name'
-  license: str = 'license'
-  pushed_at: str = 'pushed_at'
-
-  background: str = '#ff0000'
-  bar_background: str = ' #ff0000'
-  dark_keyboard: bool = True
-  default_text: str = '#ff0000'
+  url: str = 'github_url'  # 対象のVSCode Color Theme リポジトリURL
+  author: str = 'author_name'  # Theme 作者名
+  license: str = 'license'  # リポジトリに表記されているライセンス
+  pushed_at: str = 'pushed_at'  # main ブランチでの最終コミット日時
+  
+  background: str = '#ff0000'  # エディターのデフォルトの背景色
+  bar_background: str = ' #ff0000'  # ツールバーとアクティブなタブの背景色
+  dark_keyboard: bool = True  # ダーク色のオンスクリーン キーボードを使用するかどうか
+  default_text: str = '#ff0000'  # インターフェイステキストのデフォルトの色
   editor_actions_icon_background: str = '#ff0000'
   editor_actions_icon_tint: str = '#ff0000'
   editor_actions_popover_background: str = '#ff0000'
   error_text: str = '#ff0000'
-  # font_family: str = 'Menlo-Regular'
-  # font_size: float = 15.0
-  gutter_background: str = '#ff0000'
-  gutter_border: str = '#ff0000'
+  # fontFamily: str = 'Menlo-Regular'  # デフォルトのエディターおよびコンソールのフォント ファミリ
+  # fontSize: float = 15.0  # デフォルトのエディターとコンソールのフォント サイズ
+  gutter_background: str = '#ff0000'  # エディターの左側の行番号列の背景色
+  gutter_border: str = '#ff0000'  # エディターから余白を区切る境界線の色
   interstitial: str = '#ff0000'
-  library_background: str = '#ff0000'
+  library_background: str = '#ff0000'  # スクリプト ライブラリの背景色
   library_tint: str = '#ff0000'
-  line_number: str = '#ff0000'
+  line_number: str = '#ff0000'  # ガター内の行番号の色
   name: str = 'tmpFormatDump'
-
-  scopes_bold_font_style: str = 'bold'
-  scopes_bold_italic_font_style: str = 'bold-italic'
-  scopes_builtinfunction_color: str = '#ff0000'
-  scopes_checkbox_checkbox: bool = True
-  scopes_checkbox_done_checkbox: bool = True
-  scopes_checkbox_done_done: bool = True
-  scopes_class_color: str = '#ff0000'
-  scopes_classdef_color: str = '#ff0000'
-  scopes_classdef_font_style: str = 'bold'
-  scopes_code_backgroundColor: str = '#ff0000'
-  scopes_code_corner_radius: float = 2.0
-  scopes_codeblockStart_color: str = '#ff0000'
-  scopes_comment_color: str = '#ff0000'
-  scopes_comment_font_style: str = 'italic'
-  scopes_decorator_color: str = '#ff0000'
-  scopes_default_color: str = '#ff0000'
-  scopes_docstring_color: str = '#ff0000'
-  scopes_docstring_font_style: str = 'italic'
-  scopes_escape_color: str = '#ff0000'
+  # scopes: エディターでのさまざまな種類のテキストの書式設定。書式設定オプションは CSS スタイルに似ています
+  scopes_bold_fontStyle: str = 'bold'  # 太字のテキスト
+  scopes_boldItalic_fontStyle: str = 'bold-italic'  # 太字と斜体のテキスト
+  scopes_builtinfunction_color: str = '#ff0000'  # 関数呼び出しの一部としての組み込み関数
+  scopes_checkbox_checkbox: bool = True  # タスクペーパーのチェックボックス
+  scopes_checkboxDone_checkbox: bool = True  # タスクペーパーのチェックボックスをオンにします
+  scopes_checkboxDone_done: bool = True
+  scopes_class_color: str = '#ff0000'  # クラスのインスタンス化の一部としてのクラス名
+  scopes_classdef_color: str = '#ff0000'  # クラス定義の一部としてのクラス名
+  scopes_classdef_fontStyle: str = 'bold'
+  scopes_code_backgroundColor: str = '#ff0000'  # ンラインコードシーケンス
+  scopes_code_cornerRadius: float = 2.0
+  scopes_codeblockStart_color: str = '#ff0000'  # コードブロック開始後の GitHub スタイルの構文強調表示命令
+  scopes_comment_color: str = '#ff0000'  # コメント
+  scopes_comment_fontStyle: str = 'italic'
+  scopes_decorator_color: str = '#ff0000'  # 関数またはクラスの装飾ステートメント
+  scopes_default_color: str = '#ff0000'  # デフォルトのテキスト。プレーン テキスト ファイルまたは特別な意味を持たないテキストの場合
+  scopes_docstring_color: str = '#ff0000'  # Python の複数行の文字列リテラル
+  scopes_docstring_fontStyle: str = 'italic'
+  scopes_escape_backgroundColor: str = '#ff0000'  # おそらく文字列リテラル内のエスケープ文字用であるはずですが、何もしないようです
   scopes_formatting_color: str = '#ff0000'
-  scopes_function_color: str = '#ff0000'
-  scopes_functiondef_color: str = '#ff0000'
-  scopes_functiondef_font_style: str = 'bold'
-  scopes_heading1_font_style: str = 'bold'
-  scopes_heading2_font_style: str = 'bold'
-  scopes_heading3_font_style: str = 'bold'
-  scopes_italic_font_style: str = 'italic'
-  scopes_keyword_color: str = '#ff0000'
-  scopes_link_text_decoration: str = 'underline'
-  scopes_marker_box_background_color: str = '#ff0000'
-  scopes_marker_box_border_color: str = '#ff0000'
-  scopes_marker_box_border_type: int = 4
-  scopes_module_color: str = '#ff0000'
-  scopes_number_color: str = '#ff0000'
-  scopes_project_font_style: str = 'bold'
-  scopes_string_color: str = '#ff0000'
-  scopes_tag_text_decoration: str = 'none'
-  scopes_taskDone_color: str = '#ff0000'
-  scopes_taskDone_text_decoration: str = 'strikeout'
-
-  separator_line: str = '#ff0000'
-  tab_background: str = '#ff0000'
-  tab_title: str = '#ff0000'
-  text_selection_tint: str = '#ff0000'
-  thumbnail_border: str = '#ff0000'
-  tint: str = '#ff0000'
+  scopes_function_color: str = '#ff0000'  # 関数呼び出しの一部としての関数名
+  scopes_functiondef_color: str = '#ff0000'  # 関数定義の一部としての関数名
+  scopes_functiondef_fontStyle: str = 'bold'
+  scopes_heading1_fontStyle: str = 'bold'  # レベル 1 の見出し
+  scopes_heading2_fontStyle: str = 'bold'  # レベル 2 の見出し
+  scopes_heading3_fontStyle: str = 'bold'  # レベル 3 の見出し
+  scopes_italic_fontStyle: str = 'italic'  # 斜体のテキスト
+  scopes_keyword_color: str = '#ff0000'  # 言語のキーワード
+  scopes_link_textDecoration: str = 'underline'  # タイトルをリンクします
+  scopes_marker_boxBackgroundColor: str = '#ff0000'
+  scopes_marker_boxBorderColor: str = '#ff0000'
+  scopes_marker_boxBorderType: int = 4
+  scopes_module_color: str = '#ff0000'  # import ステートメントの一部としての Python モジュール名
+  scopes_number_color: str = '#ff0000'  # 数字
+  scopes_project_fontStyle: str = 'bold'  # タスクペーパープロジェクト
+  scopes_string_color: str = '#ff0000'  # 文字列リテラル
+  scopes_tag_textDecoration: str = 'none'  # これは Task paper ですか、HTML ですか、それとも何ですか?
+  scopes_taskDone_color: str = '#ff0000'  # タスクペーパーのタスクが完了しました
+  scopes_taskDone_textDecoration: str = 'strikeout'
+  
+  separator_line: str = '#ff0000'  # ツールバー/タブとエディターの間の区切り線の色
+  tab_background: str = '#ff0000'  # 非アクティブなタブの背景色
+  tab_title: str = '#ff0000'  # タブのタイトルのテキストの色
+  text_selection_tint: str = '#ff0000'  # テキスト選択とカーソルの色合い
+  thumbnail_border: str = '#ff0000'  # スクリプト ライブラリ内のファイル間の境界線の色
+  tint: str = '#ff0000'  # インターフェースボタンの色合い
 
 
 class ConvertTheme(ConvertThemeTemplate):
   pass
+
 
 class Pythonista3ThemeObject:
   """
@@ -92,7 +93,7 @@ class Pythonista3ThemeObject:
   `VSCodeThemeObject` 構成と似たような感じにするには。。。
   そもそも、構成として似せた方がええよね？
   """
-
+  
   def __init__(self):
     pass
 
@@ -107,14 +108,14 @@ class VSCodeThemeObject:
   # 今後
   API の制限回避としてローカルdump から持ってくるパターンも考えたい
   """
-
+  
   def __init__(self, github_url: str):
     self.url_path: Path = Path(github_url)
     self.name: str = self.url_path.name
     self.data: dict | None = self.__get_json_data()
     self.info: dict | None = self.__get_info_attribute()
     # xxx: `None` の時ここで弾く？
-
+  
   def to_dump(self) -> str:
     data = self.data if self.info is None else self.data | self.info
     kwargs = {
@@ -123,21 +124,21 @@ class VSCodeThemeObject:
       'ensure_ascii': False,
     }
     return json.dumps(data, **kwargs)
-
+  
   def export(self, vs_themes: Path):
     if not vs_themes.is_dir():
       vs_themes.mkdir(parents=True)
-
+    
     theme_json = self.to_dump()
     json_file = Path(vs_themes, self.name)
     json_file.write_text(theme_json, encoding='utf-8')
-
+  
   class DictDotNotation(dict):
-
+    
     def __init__(self, *args, **kwargs):
       super().__init__(*args, **kwargs)
       self.__dict__ = self
-
+  
   def __get_json_data(self) -> dict | None:
     params = {
       'raw': 'true',
@@ -147,16 +148,16 @@ class VSCodeThemeObject:
       # xxx: iceberg には、comment ない
       # wip: comment 削除処理
       return response.json()
-
+  
   def __api_tokens(self) -> dict | None:
     _, _, owner_name, repo_name, *_ = self.url_path.parts
     api_url = f'https://api.github.com/repos/{owner_name}/{repo_name}'
-
+    
     # wip: 制限かかった時の処理
     response = requests.get(api_url)
     if response.status_code == 200:
       return response.json()
-
+  
   def __get_info_attribute(self) -> DictDotNotation | None:
     tokens = self.__api_tokens()
     if tokens is None:
@@ -166,7 +167,7 @@ class VSCodeThemeObject:
     _license = l.get('name') if (l :=
                                  tokens.get('license')) is not None else str(l)
     _pushed_at = tokens.get('pushed_at')
-
+    
     # xxx: `_` は3つ
     # xxx: ↑ いや、、、不要なはず
     info = {
@@ -182,14 +183,14 @@ class ThemeInterpretation:
   """
   VSCode のTheme 情報を指定して取得
   """
-
+  
   def __init__(self, target: dict):
     self.target = target
-
+  
   def __for_colors(self, key: str) -> str | bool | int | float | None:
     # xxx: `get` じゃなくて`[key]` の方がいいか?
     return self.target['colors'].get(key)
-
+  
   def __for_token_colors(self,
                          keys: list[str]) -> str | bool | int | float | None:
     scope, settings = keys
@@ -199,21 +200,21 @@ class ThemeInterpretation:
       scopes = _scope if isinstance(_scope, list) else [_scope]
       if scope in scopes:
         return tokenColor.get('settings').get(settings)
-
+  
   def get_value(
       self,
       search_value: str = '',
       colors: str | None = None,
       tokenColors: list[str] | None = None) -> str | bool | int | float | None:
     value = None
-
+    
     if search_value:
       value = self.target.get(search_value)
     elif colors is not None and isinstance(colors, str):
       value = self.__for_colors(colors)
     elif tokenColors is not None and isinstance(tokenColors, list):
       value = self.__for_token_colors(tokenColors)
-
+    
     if value is None:
       # xxx: `raise` を正しく使いたい
       raise print(
@@ -225,10 +226,9 @@ class ThemeInterpretation:
 if __name__ == '__main__':
   target_url = 'https://github.com/cocopon/vscode-iceberg-theme/blob/main/themes/iceberg.color-theme.json'
   # target_url = 'https://github.com/cocopon/vscode-iceberg-theme/blob/main/themes/iceberg-light.color-theme.json'
-
-  #vs_theme = VSCodeThemeObject(target_url)
+  
+  # vs_theme = VSCodeThemeObject(target_url)
   # aa = to.to_dump()
   # to.export(Path('./vscodeThemes'))
-  #tp = ConvertThemeTemplate(url='hige')
-  tp = ConvertTheme(url='hoge')
-
+  # tp = ConvertThemeTemplate(url='bar')
+  tp = ConvertTheme(url='a')
