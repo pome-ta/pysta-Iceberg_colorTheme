eadf2d874ef2eaa2f0b36f590d9edacd3339f1d7
pome-ta <53405097+pome-ta@users.noreply.github.com>
2024-09-23 15:55:10 +0900

    Update 240923_1414.py

--- scripts/dists/240923_1414.py
+++ scripts/dists/240923_1414.py
@@ -297,8 +297,8 @@ class SchemaItems(SchemaTemplate):
       'text',
       'foreground',
     ])
-    self.editor_actions_icon_background = '#ff0000'#vsi.get_value(colors='badge.background')
-    self.editor_actions_icon_tint = vsi.get_value(colors='badge.foreground')
+    self.editor_actions_icon_background = vsi.get_value(colors='button.background')
+    self.editor_actions_icon_tint = vsi.get_value(colors='button.foreground')
     self.editor_actions_popover_background = vsi.get_value(
       colors='editorHoverWidget.background')
     self.error_text = vsi.get_value(colors='editorError.foreground')
