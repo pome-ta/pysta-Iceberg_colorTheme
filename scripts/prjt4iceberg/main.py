447e7e50c897a213ae3711ebbf091ebff3cc1e96
pome-ta <53405097+pome-ta@users.noreply.github.com>
2024-09-25 11:43:48 +0900

    up

--- scripts/prjt4iceberg/main.py
+++ scripts/prjt4iceberg/main.py
@@ -1,4 +1,6 @@
 if __name__ == '__main__':
   dark_url = 'https://github.com/cocopon/vscode-iceberg-theme/blob/main/themes/iceberg.color-theme.json'
   light_url = 'https://github.com/cocopon/vscode-iceberg-theme/blob/main/themes/iceberg-light.color-theme.json'
+  
+  
 
