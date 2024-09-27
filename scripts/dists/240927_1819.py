4e163c83a18a54a952e207ade239d2458cbe8c33
pome-ta <53405097+pome-ta@users.noreply.github.com>
2024-09-28 01:17:28 +0900

    up

--- scripts/dists/240927_1819.py
+++ scripts/dists/240927_1819.py
@@ -12,26 +12,31 @@ ROOT_PATH: Path = Path(__file__).parent
 class ThemeObject:
 
   def get_file_name(self, url: str) -> str | None:
-    if re.match(r'.*\.json',)
+    path = Path(url)  # xxx: 取り出し方が乱暴
+    if path.suffix == '.json':
+      return path.name
+
+  def get_tmp_dir(self, tmp_dir: Path | str | None) -> Path:
+    if tmp_dir is None:
+      return Path(ROOT_PATH, './vscodeThemes')
+    elif isinstance(tmp_dir, Path):
+      return tmp_dir
+    else:
+      return Path(tmp_dir)
 
 
-class VSCodeThemeObject:
+class VSCodeThemeObject(ThemeObject):
 
   def __init__(self,
                theme_json_url: str,
                use_local: bool = False,
-               local_dir: Path | str | None = None):
+               tmp_dir: Path | str | None = None):
     self.json_url = theme_json_url
-    '''
-    self.file_name: str | None = file_name if (
-      file_name := Path(theme_json_url).name) else None
+    self.file_name = self.get_file_name(theme_json_url)
+    self.tmp_dir = self.get_tmp_dir(tmp_dir)
+
     self.data: dict | None = None
     self.info: dict | None = None
-    self.local_dir: Path | None = None
-    '''
-    self.file_name = name if re.match(r'.*\.json',
-                                      (name := Path(theme_json_url).name),
-                                      re.IGNORECASE) else None
 
 
 if __name__ == '__main__':
